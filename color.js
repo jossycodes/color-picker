0aa160a34bd11295f004d190b2066244f136533b
josiahadeniyi1@gmail.com <josiahadeniyi1@gmail.com>
2023-04-22 22:54:51 +0100

    yeah

--- color.js
+++ color.js
@@ -408,7 +408,7 @@ code.addEventListener('keyup', (e) => {
   let val = e.target.value.trim().toUpperCase();
   code.value = val;
 
-  let chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+  let chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', '#']
   
   if (val.length == 0) return code.value = '#'
   
